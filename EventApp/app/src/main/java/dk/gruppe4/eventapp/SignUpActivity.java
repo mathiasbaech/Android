d0a07664c416956afbe52d004d97a027c43de8c0
Mathias Larsen <mathiasbaech@outlook.com>
2014-12-16 13:38:10 +0100

    kalenderfis

--- EventApp/app/src/main/java/dk/gruppe4/eventapp/SignUpActivity.java
+++ EventApp/app/src/main/java/dk/gruppe4/eventapp/SignUpActivity.java
@@ -4,16 +4,62 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.app.DialogFragment;
+
+import java.util.Calendar;
 
 
 public class SignUpActivity extends ActionBarActivity {
+
+
+
+
+
+
+    private TextView mDateDisplay;
+    private Button mPickDate;
+    static final int DATE_DIALOG_ID = 0;
+
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
+
+        mDateDisplay = (TextView) findViewById(R.id.showMyDate);
+        mPickDate = (Button) findViewById(R.id.myDatePickerButton);
+
+        mPickDate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG_ID);
+            }
+        });
+
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        updateDisplay();
     }
 
+    private void updateDisplay() {
+        this.mDateDisplay.setText(
+                new StringBuilder()
+                        // Month is 0 based so add 1
+                        .append(mMonth + 1).append("-")
+                        .append(mDay).append("-")
+                        .append(mYear).append(" "));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
